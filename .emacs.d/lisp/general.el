554b6ff39e81d7f50f09302543e1d8866dd0b739
Erwin-zhu <erwin_zhu@163.com>
2014-11-25 17:24:47 +0800

    solve the problem about java-mode indentation
    example:
    public testFunctin()
    {
    }
    change this ugly style.

--- .emacs.d/lisp/general.el
+++ .emacs.d/lisp/general.el
@@ -21,14 +21,6 @@
 ;;禁止自动备份
 (setq make-backup-files nil)
 
-;;解决缩进为8个字符问题
-(setq-default tab-width 4)
-;;编程时缩进为4个字符（默认为2个字符，在c，c++编程是，默认2个字符很难看）
-;;参考资料: emacs中cc-mode的缩进配置方法详解
-;; http://hi.baidu.com/susdisk/item/4a33cd87e45b74dfd0f8cd32
-(setq c-basic-offset 4)
-(setq c-default-style "linux")
-
 ;;在标题栏上显示buffer的名字
 (setq frame-title-format "emacs@%b")
 
--- .emacs.d/lisp/programing.el
+++ .emacs.d/lisp/programing.el
@@ -1,3 +1,4 @@
+
 ;;
 ;;在特定模式下粘贴自动缩进
 (defadvice yank (after indent-region activate)
@@ -15,5 +16,14 @@
 
 ;; --- 自动对 C 语言的类型定义识别并进行语法加亮
 ;;(ctypes-auto-parse-mode 1)
+
+;;解决缩进为8个字符问题
+(setq-default tab-width 4)
+;;编程时缩进为4个字符（默认为2个字符，在c，c++编程是，默认2个字符很难看）
+;;参考资料: emacs中cc-mode的缩进配置方法详解
+;; http://hi.baidu.com/susdisk/item/4a33cd87e45b74dfd0f8cd32
+(setq c-basic-offset 4)
+(setq c-default-style "linux")
+(c-set-offset 'inline-open())                   ;;写java时，函数完成后ret后，默认不缩进
 
 (provide 'programing)
