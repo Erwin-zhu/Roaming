8bd248b834ac4241aaf53ffb35f0bce42d7a5136
Erwin-zhu <erwin_zhu@163.com>
2014-12-02 17:48:07 +0800

    add C-x M-d to print time

--- .emacs.d/lisp/myPlugin.el
+++ .emacs.d/lisp/myPlugin.el
@@ -108,4 +108,16 @@
                               yas-ido-prompt
                               yas-completing-prompt))
 
+;; calendar
+(require 'calendar)
+(defun insdate-insert-current-date (&optional omit-day-of-week-p)
+  "Insert today's date using the current locale.
+  With a prefix argument, the date is inserted without the day of
+  the week."
+  (interactive "P*")
+  (insert (calendar-date-string (calendar-current-date) nil
+								omit-day-of-week-p)))
+(global-set-key "\C-x\M-d" `insdate-insert-current-date)
+(calendar-set-date-style 'iso)
+
 (provide 'myPlugin)
